7a29d539815bffdb6f93a426383a4e02b628aa5a
Joseph Ortiz <josepho011235@gmail.com>
2014-08-25 14:19:41 -0400

    added basic shopping list code

---
+++ js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(event) {
+    
+    $("#addButton").click(function() {
+        addItem();
+    });
+
+    $("#itemInput").keypress(function(event) {
+        if (event.keyCode == 13) {
+            addItem();
+        }
+    });
+
+});
+
+function addClickHandler() {
+    $("li.item").dblclick(function() { //toggles list item with a strikeout.
+        var item = $(this);
+        if (item.parent().is("strike")) {
+            $(item).unwrap();
+        } else {
+            $(item).wrap("<strike>");
+        }
+    });
+
+    $("a.item-close").click(function(event) { //removes item from list
+        $(this).parent("li").remove();
+    });
+}
+
+function addItem() {
+    var newItem = $("#itemInput").val();
+    if ((newItem !== "") && (newItem !== null)) {
+        var newItemNode = "<li class='item'>" + newItem + "<a class='item-close'>close</a></li>";
+        $("#shoppingList").append(newItemNode);
+        $("li.item").unbind();
+        addClickHandler();
+    } else {
+        alert("No value found");
+    }
+}
