acd155af7d37e8a3799e382f2d47b595ce364961
Joseph Ortiz <josepho011235@gmail.com>
2014-08-27 00:39:39 -0400

    added mark all complete feature

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
 $(document).ready(function(event) {
-
     $("#addButton").click(function() {
         addItem();
     });
-
     $("#itemInput").keypress(function(event) {
         if (event.keyCode == 13) {
             addItem();
         }
     });
-
+    addMarkAllItemsCompleteEvent();
 });
 
 function addClickHandler() {
 
     $(".item").click(function() { //toggles list item with a strikeout.
         var item = $(this);
-
         if ($(item).hasClass('strike-through')) {
-            $(item).removeClass('strike-through');
+            markItemIncomplete(item);
         } else {
-            $(item).addClass('strike-through');
+            markItemCompleted(item);
         }
     });
 
     $("a.item-close").click(function(event) { //removes item from list
         $(this).parent("li").remove();
     });
+}
+
+function markItemCompleted(li) {
+    $(li).addClass('strike-through');
+}
+
+function markItemIncomplete(li) {
+    $(li).removeClass('strike-through');
 }
 
 function addItem() {
@@ -40,4 +45,28 @@ function addItem() {
     } else {
         alert("No value found");
     }
+}
+
+function addMarkAllItemsCompleteEvent() {
+    var button = $(".mark-all-complete");
+    $(button).click(function() {
+        var areAllItemsChecked = $(button).attr("toggle-checked");
+        var listItems = $("#shoppingList").find("li");
+        if (areAllItemsChecked === "true") {
+            $(listItems).each(function(index) {
+                markItemIncomplete(listItems[index]);
+            });
+            $(button).text("Mark All Complete");
+            $(button).parent().removeClass("marked");
+            $(button).attr("toggle-checked", "false");
+        } else {
+            $(listItems).each(function(index) {
+                markItemCompleted(listItems[index]);
+            });
+            $(button).text("Undo All");
+            $(button).parent().addClass("marked");
+            $(button).attr("toggle-checked", "true");
+        }
+    });
+
 }
